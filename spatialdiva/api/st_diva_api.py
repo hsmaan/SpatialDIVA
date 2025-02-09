63c9e208238c5dee27638374a833f9890ba6273b
hsmaan <hassaan.maan@mail.utoronto.ca>
2025-02-09 16:03:23 -0500

    api update

--- spatialdiva/api/st_diva_api.py
+++ spatialdiva/api/st_diva_api.py
@@ -7,6 +7,11 @@ import pandas.api.types as ptypes
 import torch
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import wandb    
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch import Trainer
+from sklearn.decomposition import PCA
+import umap
 
 from utils.diva_data import find_knn, return_dataset, dataset_to_dataloader, adata_process
 from models import SpatialDIVA, LitSpatialDIVA
@@ -147,7 +152,7 @@ class StDIVA:
         neighbor_data_tensor = torch.from_numpy(neighbor_data)
         spatial_positions_tensor = torch.from_numpy(spatial_positions)
         
-        train_dataset = torch.utils.data.TensorDataset(
+        self.train_dataset = torch.utils.data.TensorDataset(
             count_hist_data_tensor,
             st_labels_tensor,
             morpho_labels_tensor,
@@ -156,20 +161,145 @@ class StDIVA:
             spatial_positions_tensor
         )
         
-        self.train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
+        self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=128, shuffle=True)
         
         # Do the same for the validation data
         if self.val_data is not None:
-            count_hist_data_tensor = torch.from_numpy(self.val_data.X)
-            st_labels_tensor = torch.from_numpy(process_labels(self.val_data.obs[label_key_y1], "categorical"))
-            morpho_labels_tensor = torch.from_numpy(process_labels(self.val_data.obs[pos_key], "categorical"))
-            batch_labels_tensor = torch.from_numpy(process_labels(self.val_data.obs["batch"], "categorical"))
+            count_data_val = np.asarray(self.val_data.X)
+            hist_cols_val = [col for col in self.val_data.obs.columns if hist_col_key in col]
+            hist_data_val = self.val_data.obsm[hist_cols_val].values
             
-            # 
+            count_hist_data_val = np.concatenate([count_data_val, hist_data_val], axis=1)
             
-            self.val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=128, shuffle=False)
+            st_labels_val = process_labels(self.val_data.obs[label_key_y1], "categorical")
+            morpho_labels_val = process_labels(self.val_data.obs[pos_key], "categorical")
+            
+            num_classes_morpho = len(np.unique(morpho_labels_val))
+            morpho_labels_onehot_val = np.eye(num_classes_morpho)[morpho_labels_val]
+            
+            num_classes_st = len(np.unique(st_labels_val))
+            st_labels_onehot_val = np.eye(num_classes_st)[st_labels_val]
+            
+            batch_labels_val = process_labels(self.val_data.obs["batch"], "categorical")
+            num_classes_batch = len(np.unique(batch_labels_val))
+            batch_labels_onehot_val = np.eye(num_classes_batch)[batch_labels_val]   
+            
+            count_hist_data_val_tensor = torch.from_numpy(count_hist_data_val)
+            st_labels_val_tensor = torch.from_numpy(st_labels_onehot_val)
+            morpho_labels_val_tensor = torch.from_numpy(morpho_labels_onehot_val)
+            batch_labels_val_tensor = torch.from_numpy(batch_labels_onehot_val)
+            
+            neighbor_data_val = self.val_data.obsm["X_pca_neighbors_avg"]
+            neighbor_data_val_tensor = torch.from_numpy(neighbor_data_val)
+            
+            spatial_positions_val = self.val_data.obsm["spatial"]
+            spatial_positions_val_tensor = torch.from_numpy(spatial_positions_val)  
+            
+            self.val_dataset = torch.utils.data.TensorDataset(
+                count_hist_data_val_tensor,
+                st_labels_val_tensor,
+                morpho_labels_val_tensor,
+                batch_labels_val_tensor,
+                neighbor_data_val_tensor,
+                spatial_positions_val_tensor
+            )
+            
+            self.val_loader = torch.utils.data.DataLoader(self.val_dataset, batch_size=128, shuffle=False)
      
     def train(self, max_epochs = 100, early_stopping = True, patience = 10):
-        # Train the model
-        train_loader = 
+        # Train the model using torch lightning 
+        trainer = Trainer(
+            max_epochs = max_epochs,
+            callbacks = [EarlyStopping(monitor="val_loss", patience=patience)] if early_stopping else None
+        )
+        
+        trainer.fit(self.model, self.train_loader, self.val_loader)
+        
+    def save_model(self, path):
+        torch.save(self.model.state_dict(), path)
 
+    def load_model(self, path):
+        self.model.load_state_dict(torch.load(path))
+        
+        
+    def get_embeddings(self, type = "train"):
+        if type == "train":
+            loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=128, shuffle=False)
+        elif type == "val":
+            loader = torch.utils.data.DataLoader(self.val_dataset, batch_size=128, shuffle=False)
+        else:
+            raise ValueError("Invalid type")
+        
+        # Get the posterior embeddings for the data 
+        self.model.eval()
+        y1_labels = []
+        y2_labels = []
+        y3_labels = []
+        d_labels = []
+        zy1_samples = []
+        zy2_samples = []
+        zy3_samples = []
+        zd_samples = []
+        zx_samples = []
+            
+        with torch.no_grad():
+            for batch in loader:
+                x, y1, y2, y3, d, spo_var = batch
+                x = x.double()
+                y1 = y1.double()
+                y2 = y2.double()
+                y3 = y3.double()
+                d = d.double()
+                d = d.unsqueeze(dim=-1)
+
+                y = [y1, y2, y3]
+                zx, zys, zd = self.model.get_posterior(x)
+
+                # Detach, move to CPU and convert to numpy
+                y1 = y1.detach().cpu().numpy()
+                y2 = spo_var.detach().cpu().numpy()
+                y3 = y3.detach().cpu().numpy()
+                d = d.detach().cpu().numpy()
+                zd = zd.detach().cpu().numpy()
+                zx = zx.detach().cpu().numpy()
+                zys = [zy.detach().cpu().numpy() for zy in zys]
+
+                # Append the labels
+                y1_labels.append(y1)
+                y2_labels.append(y2)
+                y3_labels.append(y3)
+                d_labels.append(d)
+                zd_samples.append(zd)
+                zy1_samples.append(zys[0])
+                zy2_samples.append(zys[1])
+                zy3_samples.append(zys[2])
+                zx_samples.append(zx)
+                    
+        y1_labels = np.concatenate(y1_labels, axis=0)
+        y2_labels = np.concatenate(y2_labels, axis=0)
+        y3_labels = np.concatenate(y3_labels, axis=0)
+        d_labels = np.concatenate(d_labels, axis=0)
+        zd_samples = np.concatenate(zd_samples, axis=0)
+        zy1_samples = np.concatenate(zy1_samples, axis=0)
+        zy2_samples = np.concatenate(zy2_samples, axis=0)
+        zy3_samples = np.concatenate(zy3_samples, axis=0)
+        zx_samples = np.concatenate(zx_samples, axis=0)
+        
+        # Return the embeddings 
+        return y1_labels, y2_labels, y3_labels, d_labels, zd_samples, zy1_samples, zy2_samples, \
+            zy3_samples, zx_samples
+        
+    def reduce_embedding(self, embedding, method = "pca"):
+        if method == "pca":
+            pca = PCA(n_components=2)
+            return pca.fit_transform(embedding  )
+        elif method == "umap":
+            reducer = umap.UMAP(n_components=2)
+            return reducer.fit_transform(embedding)
+        else:
+            raise ValueError("Invalid method")
+        
+        
+    
+        
+        
